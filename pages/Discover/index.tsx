04b4934821380685b5c09c0e4057bdd13373c17a
rei <lamduchoa96@gmail.com>
2022-10-26 23:53:58 +0700

    t

--- pages/Discover/index.tsx
+++ pages/Discover/index.tsx
@@ -725,9 +725,9 @@ const Discover = (props: DiscoverProps) => {
                       width="600"
                       height="450"
                       style={{ border: "0" }}
-                      allowFullScreen=""
+                      // allowFullScreen=""
                       loading="lazy"
-                      referrerpolicy="no-referrer-when-downgrade"
+                      // referrerpolicy="no-referrer-when-downgrade"
                     ></iframe>
                   </div>
                 </div>
@@ -743,9 +743,9 @@ const Discover = (props: DiscoverProps) => {
                   width="600"
                   height="450"
                   style={{ border: "0" }}
-                  allowFullScreen=""
+                  // allowFullScreen=""
                   loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade"
+                  // referrerpolicy="no-referrer-when-downgrade"
                 ></iframe>
               </div>
             </div>
