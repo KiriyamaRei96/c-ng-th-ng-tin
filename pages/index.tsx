d1ab5ceafa5c45f18500a69ec9faf8ad7a59acf4
Hoang-Fe <lamduchoa96@gmail.com>
2022-11-02 17:26:21 +0700

    tesst 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,17 +24,15 @@ import callApi from "../Api/Axios";
 import axios from "axios";
 
 export async function getStaticProps() {
-  const res = await fetch(
-    `https://demo2.starfruit.com.vn/api/v2/page/Home?locale=vi`,
-    {
-      headers: { "x-token": process.env.DB_X_TOKEN },
-    }
-  )
+  const api = process.env.DB_API;
+  const res = await fetch(`${api}/v2/page/Home?locale=vi`, {
+    headers: { "x-token": process.env.DB_X_TOKEN },
+  })
     .then((res) => res.json())
     .then((res) => res.data)
     .catch((err) => console.error(err));
 
-  const homeBanner = res.snippets.find(
+  const homeBanner = res?.snippets?.find(
     (item) => item["snippet_name"] === "HomeBanner"
   );
 
