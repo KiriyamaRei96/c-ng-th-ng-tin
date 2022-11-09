91bee561d80b78d6f6759cd46abc881f84786429
rei <lamduchoa96@gmail.com>
2022-11-09 20:25:05 +0700

    push

--- components/Layout/Footer/component/FooterForm.tsx
+++ components/Layout/Footer/component/FooterForm.tsx
@@ -1,38 +1,58 @@
+import Link from "next/link";
 import React from "react";
 
 const FooterForm = ({ footerForm }) => {
+  const facebook = footerForm?.articles?.find(
+    (item) => item.title === "facebook"
+  );
+  const twitter = footerForm?.articles?.find(
+    (item) => item.title === "twitter"
+  );
+  const instagram = footerForm?.articles?.find(
+    (item) => item.title === "instagram"
+  );
+  const google = footerForm?.articles?.find((item) => item.title === "google");
+
   return (
-    <div className='footer_form'>
-      <h4 className='--title'>{footerForm?.title}</h4>
-      <div className='--des'>{footerForm?.subTitle}</div>
-      <div className='--email'>
-        <input type='text' placeholder='email' />
+    <div className="footer_form">
+      <h4 className="--title">{footerForm?.title}</h4>
+      <div className="--des">{footerForm?.subTitle}</div>
+      <div className="--email">
+        <input type="text" placeholder="email" />
         <button>
-          <i className='fa-solid fa-arrow-right-long'></i>
+          <i className="fa-solid fa-arrow-right-long"></i>
         </button>
       </div>
-      <div className='--follow'>
+      <div className="--follow">
         <span>Kết nối:</span>
         <ul>
           <li>
-            <a href=''>
-              <i className='fa-brands fa-facebook'></i>
-            </a>
+            <Link href={facebook?.link ? facebook?.link : ""}>
+              <a>
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+            </Link>
           </li>
           <li>
-            <a href=''>
-              <i className='fa-brands fa-twitter'></i>
-            </a>
+            <Link href={twitter?.link ? twitter?.link : ""}>
+              <a>
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+            </Link>
           </li>
           <li>
-            <a href=''>
-              <i className='fa-brands fa-instagram'></i>
-            </a>
+            <Link href={instagram?.link ? instagram?.link : ""}>
+              <a>
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+            </Link>
           </li>
           <li>
-            <a href=''>
-              <i className='fa-brands fa-google'></i>
-            </a>
+            <Link href={google?.link ? google?.link : ""}>
+              <a>
+                <i className="fa-brands fa-google"></i>
+              </a>
+            </Link>
           </li>
         </ul>
       </div>
--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -39,10 +39,10 @@ const Footer = ({ iconSlider }) => {
 
   return (
     <footer>
-      <div className='footer_Logo'>
-        <div className='container-fluid'>
+      <div className="footer_Logo">
+        <div className="container-fluid">
           <Slider
-            className='list_logo'
+            className="list_logo"
             {...{
               arrows: false,
               dots: false,
@@ -55,40 +55,40 @@ const Footer = ({ iconSlider }) => {
           >
             {slider?.articles.map((item) => (
               <div key={uuid()}>
-                <div className='--img'>
-                  <img src={item.image?.path} alt='' />
+                <div className="--img">
+                  <img src={item.image?.path} alt="" />
                 </div>
               </div>
             ))}
           </Slider>
-          <div className='list_logo d-flex'></div>
+          <div className="list_logo d-flex"></div>
         </div>
       </div>
-      <div className='container-fluid'>
-        <div className='footer d-flex justify-content-between'>
+      <div className="container-fluid">
+        <div className="footer d-flex justify-content-between">
           <FooterInfo footerInfo={footerInfo} />
           <FooterLink footerLink={footerLink} />
           <FooterLink footerLink={footerPrivacy} />
           <FooterForm footerForm={footerForm} />
         </div>
       </div>
-      <div className='footer_bot'>
-        <div className='container-fluid'>
-          <div className='--content d-flex justify-content-between'>
-            <div className='--left d-flex align-items-center'>
+      <div className="footer_bot">
+        <div className="container-fluid">
+          <div className="--content d-flex justify-content-between">
+            <div className="--left d-flex align-items-center">
               <span>@Du Lịch Lai Châu 2022 by Starfruit</span>
-              <div className='--link d-flex align-items-center'>
-                <a href=''>Privacy Cookies Policy</a>
-                <a href=''>Terms and Conditions</a>
+              <div className="--link d-flex align-items-center">
+                <a href="">Privacy Cookies Policy</a>
+                <a href="">Terms and Conditions</a>
               </div>
             </div>
-            <div className='--right d-flex align-items-center'>
-              <div className='--item d-flex align-items-center'>
-                <img src={globe.src} alt='' />
+            <div className="--right d-flex align-items-center">
+              <div className="--item d-flex align-items-center">
+                <img src={globe.src} alt="" />
                 Đang online: <span>18</span>
               </div>
-              <div className='--item d-flex align-items-center'>
-                <img src={chart.src} alt='' />
+              <div className="--item d-flex align-items-center">
+                <img src={chart.src} alt="" />
                 Tổng: <span>9886</span>
               </div>
             </div>
