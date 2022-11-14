c641f53b73b5fc7e3d5c1f524555864912b6d632
Hoang-Fe <lamduchoa96@gmail.com>
2022-11-14 17:57:07 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* <Provider store={store}> */}
+
       <link
         rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
